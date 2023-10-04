387a4988e74a5821d52ef05d465d866a1d32724d
Andres Grover Albino <grover.albino@gmail.com>
2023-10-03 21:17:35 -0400

    Cambios finales de la app

--- src/app/components/pages/characters/character-details/character-details.component.ts
+++ src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Character } from '@app/shared/interface/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
   styleUrls: ['./character-details.component.scss']
 })
-export class CharacterDetailsComponent {
+export class CharacterDetailsComponent implements OnInit {
 
+  // Variable
+  character$!: Observable<Character>;
+
+  constructor(private route: ActivatedRoute,
+              private characterService: CharacterService,
+              private location: Location) {}
+
+  ngOnInit(): void {
+    //INVOCAMOS AL MÉTODO DE OBTENCION DE UN PERSONAJE
+    this.route.params.pipe(take(1)).subscribe((params)=>{
+      const id = params['id'];
+      this.character$ = this.characterService.getDetails(id);
+    });
+  }
+
+  // MÉTODO PARA RETORNAR AL LISTADO COMPLETO DE PERSONAJES
+  onGoBack() {
+    this.location.back();
+  }
 }
